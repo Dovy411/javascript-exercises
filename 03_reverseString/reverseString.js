8f9fdc3af356b022fea7cbb63a7b6b5bdd01e62c
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-20 11:03:55 +0100

    Finished exercise 3. Create a for loop to reverse string rather than using in-built functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
 
+    for (let i  = (string.length)-1; i>=0; i--) {
+        newString += string[i];
+    }
+
+    return newString;
 };
+
+console.log(reverseString("Hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
