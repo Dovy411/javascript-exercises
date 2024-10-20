9c65a02bf573871a9e74b00fd8c86f4b09358854
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-20 16:47:57 +0100

    Not sure how filter() and ...args work. Found the complete solution by accident. Learn filter() and ...args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    // let indexRemove = list.indexOf(itemArray);
+
+    // list.splice(indexRemove, 1);
+
+    // console.log(itemArray)
+    //return list;
+
+    return array.filter(x => !args.includes(x));
 };
+
+
+console.log(removeFromArray([1,2,3,4], 2, 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
