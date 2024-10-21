ceaf1a4d7fd8ebb81717e79e0e45a16062870c9f
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-21 18:24:46 +0100

    Passed the first 3 tests. I think there's a better way to do this, however.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
+    let finalSum = 0;
+    
+    if (numOne < numTwo) {
+        for (let i=numOne; i<=numTwo; i++) {finalSum += i;};
+        return finalSum;
+    } else {
+        for (let i=numTwo; i<=numOne; i++) {finalSum += i;};
+        return finalSum;
+    }
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
