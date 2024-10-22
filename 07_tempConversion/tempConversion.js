4beb323d08be9b027c71e9db8e00886be5ffcc95
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-22 16:13:53 +0100

    Passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f-32)*5/9;
+  c = c.toFixed(1);
+  c = Number(c);
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (c*9/5)+32;
+  f = f.toFixed(1);
+  f = Number(f);
+  return f;
 };
 
 // Do not edit below this line
@@ -9,3 +17,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
