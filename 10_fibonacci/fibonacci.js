165b94ad0fe5779fdbec287f44f1dca5a5a4eb46
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-24 12:37:53 +0100

    Passed all tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if (number<0) return "OOPS";
+
+    if (typeof number === "string") {
+        number = parseInt(number);
+    }
+
+    let num = 1;
+    let sum = 0;
+    let oldSum = 0;
+
+    for (let i = 1; i <= number; i++) {
+        oldSum = sum;
+        sum += num;
+        num = oldSum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// npm test fibonacci.spec.js
+
+console.log(fibonacci(4))
