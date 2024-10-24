63bb2c67fe8391429c1f17c47cb465a1fd7014b7
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-24 10:18:53 +0100

    Finished all tasks

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let newWord = word.replace(/[\.,?!]/g, '');
+    let revWord = newWord.split("").reverse().join("");
+    revWord = revWord.toLowerCase().replace(/\s/g, '');
+    newWord = newWord.toLowerCase().replace(/\s/g, '');
+    if (revWord == newWord) {
+        return true;
+    }
 
+    return false;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// npm test palindromes.spec.js
+
+console.log(palindromes("Racecar!"));
