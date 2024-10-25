117c56a5e385e7b6c56ece4d2380c0d49eb0a062
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-25 11:35:39 +0100

    Finished, didn't use built-in js method.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,33 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
 
+const getTheTitles = function(books) {
+
+    let theTitles = [];
+
+    for (let i = 0; i < books.length; i++) {
+        theTitles.push(books[i].title);
+    }
+
+    return theTitles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// The solutions
+
+// const getTheTitles = function (array) {
+//     return array.map((book) => book.title);
+//   };
+  
+//   module.exports = getTheTitles;
+  
